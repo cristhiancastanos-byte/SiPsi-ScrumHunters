cc456f527ac9782c95a2552b36f00afa0b261ddd
Yael_ <cristhian.castanos@uabc.edu.mx>
2026-04-04 17:59:05 -0700

    agrega lógica de filtrado - Cristhian

--- negocio/src/main/java/mx/sipsi/negocio/integration/IPacienteNegocioIntegration.java
+++ negocio/src/main/java/mx/sipsi/negocio/integration/IPacienteNegocioIntegration.java
@@ -1,7 +1,11 @@
 package mx.sipsi.negocio.integration;
 
 import mx.sipsi.entity.PacienteEntity;
+import java.util.List;
 
 public interface IPacienteNegocioIntegration {
     void llamarIntegracion(PacienteEntity paciente) throws Exception;
+
+    List<PacienteEntity> buscarTodosActivos();
+    List<PacienteEntity> buscarPorNombreActivos(String nombre);
 }
