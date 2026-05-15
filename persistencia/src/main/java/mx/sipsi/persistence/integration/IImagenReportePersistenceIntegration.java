7e1a2f44d4fc8eee43739bb8d5b16844fd564843
Yael_ <cristhian.castanos@uabc.edu.mx>
2026-05-15 00:14:01 -0700

    Agregar persistencia para imagenes de reporte - Cristhian

---
+++ persistencia/src/main/java/mx/sipsi/persistence/integration/IImagenReportePersistenceIntegration.java
@@ -0,0 +1,11 @@
+package mx.sipsi.persistence.integration;
+
+import mx.sipsi.entity.ImagenReporteEntity;
+import java.util.List;
+
+public interface IImagenReportePersistenceIntegration {
+
+    void guardarImagen(ImagenReporteEntity imagen) throws Exception;
+
+    List<ImagenReporteEntity> listarImagenesPorReporte(Integer idReporte) throws Exception;
+}
